30b99269554c9bdbb793698ec2d006375e6e6828
fabricioFalcao <fabriciohfd@gmail.com>
2023-05-13 18:23:45 -0300

    Bonus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,55 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+const verifyLength = (numbersArray) => numbersArray.length === 11;
+
+const verifyDigits = (numbersArray) => {
+  for (let value of numbersArray) {
+    if (value > 9 || value < 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const verifyRepeated = (numbersArray) => {
+  let contador = {};
+  for (let i = 0; i < numbersArray.length; i += 1) {
+    let atual = numbersArray[i];
+    if (contador[atual] === undefined) {
+      contador[atual] = 1;
+    } else {
+      contador[atual] += 1;
+    }
+  }
+  return contador;
+};
+
+const verifyRepetition = (numbersArray) => {
+  let repet = verifyRepeated(numbersArray);
+
+  for (let i in repet) {
+    if (repet[i] >= 3) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generatePhoneNumber = (numbersArray) => {
+  if (!verifyLength(numbersArray)) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (!verifyDigits(numbersArray) || !verifyRepetition(numbersArray)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  else {
+    numbersArray.splice(0, 0, '(');
+    numbersArray.splice(3, 0, ') ');
+    numbersArray.splice(9, 0, '-');
+    return numbersArray.join('');
+  }
+};
+
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
@@ -8,7 +58,7 @@
 
 // Não modifique essas linhas
 module.exports = {
-  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => {}),
-  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => {}),
-  hydrate: typeof hydrate === 'function' ? hydrate : (() => {}),
+  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => { }),
+  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => { }),
+  hydrate: typeof hydrate === 'function' ? hydrate : (() => { }),
 };
